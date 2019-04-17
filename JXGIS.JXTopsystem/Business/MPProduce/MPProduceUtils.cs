c93baef118451c7f5f106762686451c13b1765b4
Administrator <Administrator@Z8OYCT72GQFXSTU>
2019-04-17 15:11:19 +0800

    11

--- JXGIS.JXTopsystem/Business/MPProduce/MPProduceUtils.cs
+++ JXGIS.JXTopsystem/Business/MPProduce/MPProduceUtils.cs
@@ -758,15 +758,15 @@ t1 as(
                     }
                     sql = sql.Substring("union ".Length);
                     query = $@"with t as({sql}),
- t1 as(
-  select t.PLID,IsNull(t.SBDW, '空') SBDW,IsNull(t.ResidenceName, '空') ResidenceName,IsNull(t.Applicant, '空') Applicant,IsNull(t.ApplicantPhone, '空') ApplicantPhone,CONVERT(varchar(100),t.BZTime, 23) BZTime from t 
+  t1 as(
+  select t.PLID,IsNull(t.SBDW, '空') SBDW,IsNull(t.ViligeName, '空') ViligeName,IsNull(t.Applicant, '空') Applicant,IsNull(t.ApplicantPhone, '空') ApplicantPhone,CONVERT(varchar(100),t.BZTime, 23) BZTime from t 
   where t.mpproduce=1 and t.AddType='批量' and t.PLProduceID is null and t.State=1
-  group by t.PLID,t.SBDW,t.ResidenceName,t.Applicant,t.ApplicantPhone,t.BZTime
+  group by t.PLID,t.SBDW,t.ViligeName,t.Applicant,t.ApplicantPhone,t.BZTime
  ),
  t2 as(
- select t1.plid,'申报单位:'+t1.SBDW+'|'+'小区名称:'+t1.ResidenceName+'|'+'申办人:'+t1.Applicant+'|'+'联系电话:'+t1.ApplicantPhone+'|'+'编制时间:'+t1.BZTime ct from t1
+ select t1.plid,'申报单位:'+t1.SBDW+'|'+'自然村名称:'+t1.ViligeName+'|'+'申办人:'+t1.Applicant+'|'+'联系电话:'+t1.ApplicantPhone+'|'+'编制时间:'+t1.BZTime ct from t1
  )
- select t2.PLID, STUFF((select ';' + tt.ct from  t2 tt where tt.PLID = t2.PLID for xml path('')),1,1,'') Content from t2
+ select t2.PLID, STUFF((select ';' + tt.ct from  t2 tt where tt.PLID = t2.PLID for xml path('')),1,1,'') ct from t2
  group by t2.plid";
                 }
                 else
